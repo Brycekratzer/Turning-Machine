34930860d6d8e5a7b524f9c5cba380b46ff16cd5
bryce Kratzer <brycekratzer@u.boisestate.edu>
2025-04-07 17:45:51 -0600

    Starting Simulator

---
+++ TMSimulator.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ * @Brycekratzer
+ * 
+ * The TMState class represents a state inside of a Turning Machine
+ */
+class TMState {
+    private int state;
+
+    /**
+     * @constructor 
+     * 
+     * @param state
+     * Takes an integer for the State class that represents it's state
+     * id. 
+     */
+    public TMState(int state){
+        this.state = state;
+    }
+}
+
+/**
+ * 
+ * @Brycekratzer
+ * 
+ * The TM class represents a working deterministic Turning Machine
+ */
+class TM {
+
+}
+
+public class TMSimulator{
+    public static void main(String[] args) {
+        
+    }
+}
