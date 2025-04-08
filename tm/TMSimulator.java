06991a7068032527e94d674e13bea7f546e5d431
bryce Kratzer <brycekratzer@u.boisestate.edu>
2025-04-08 16:42:17 -0600

    Bug fix

--- tm/TMSimulator.java
+++ tm/TMSimulator.java
@@ -22,36 +22,35 @@ public class TMSimulator{
         // The LAST STATE, DOES NOT, have ANY EXITING TRANSITIONS
 
         // Initializing the Turning Machine by getting the number of states and characters
-        Scanner fileReader; 
-        try(fileReader = new Scanner(new File(filePath))) {
+        try(Scanner fileReader = new Scanner(new File(filePath))) {
             String line = fileReader.nextLine();
             numStates = Integer.parseInt(line);
             line = fileReader.nextLine();
             numChars = Integer.parseInt(line);
             tm = new TM(numStates, numChars);
+    
+            // Initialzing the state transitions
+            int lineIndex = 0;
+            int transitionsPerState = numChars + 1;
+            int totalTransitions = (numStates - 1) * (numChars + 1);
+            while (fileReader.hasNextLine()){
+                String transitionline = fileReader.nextLine();
+    
+                // Get each part from line
+                String[] parts = line.split(",");
+    
+                // Calculate current state and index
+                int currentStateIndex = lineIndex / transitionsPerState;
+                int currentCharIndex = lineIndex % transitionsPerState;
+    
+                // Get parts from line
+                int nextState = Integer.parseInt(parts[0]);
+                int writeChar = parts[1].charAt(0);
+                int moveDirection = parts[2].charAt(0) == 'R' ? 1 : 0;
+    
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }
-
-        // Initialzing the state transitions
-        int lineIndex = 0;
-        int transitionsPerState = numChars + 1;
-        int totalTransitions = (numStates - 1) * (numChars + 1);
-        while (fileReader.hasNextLine()){
-            String line = fileReader.nextLine();
-
-            // Get each part from line
-            String[] parts = line.split(",");
-
-            // Calculate current state and index
-            int currentStateIndex = lineIndex / transitionsPerState;
-            int currentCharIndex = lineIndex % transitionsPerState;
-
-            // Get parts from line
-            int nextState = Integer.parseInt(parts[0]);
-            int writeChar = parts[1].charAt(0);
-            int moveDirection = parts[2].charAt(0) == 'R' ? 1 : 0;
-
         }
     }
 }
